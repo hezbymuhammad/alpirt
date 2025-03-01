c36897f1baf4286dd32a2c5bd706daf643aa6725
hezbymuhammad <hezbymuhammad@gmail.com>
2025-03-01 16:44:18 +0700

    feat: add user circle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_many :sleep_histories
 
+  has_many :follower_circles, class_name: 'Circle', foreign_key: 'following_id', dependent: :destroy
+  has_many :following_circles, class_name: 'Circle', foreign_key: 'user_id', dependent: :destroy
+  has_many :followers, through: :follower_circles, source: :user
+  has_many :followings, through: :following_circles, source: :following
+
   def sleep(sleep_time = DateTime.current)
     sleep_histories.create(
       clock_in: sleep_time
@@ -12,4 +17,18 @@ class User < ApplicationRecord
       clock_out: wake_up_time
     )
   end
+
+  def follow(user)
+    following_circles.find_or_create_by(following: user)
+  end
+
+  def unfollow(user)
+    circle = following_circles.find_by(following: user)
+    circle&.destroy
+  end
+
+  def following?(user)
+    following_circles.where(following: user).exists?
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { create :user }
+  let(:follower) { create :user }
 
   describe "#sleep" do
     it "create sleep record with default value" do
@@ -36,6 +37,43 @@ RSpec.describe User, type: :model do
 
       sleep_history = user.reload.sleep_histories.last
       expect(sleep_history.clock_out).to eq(time)
+    end
+  end
+
+  describe "#follow" do
+    it "able to follow user" do
+      user
+      follower
+
+      follower.follow(user)
+
+      expect(follower.followings.last).to eq(user)
+    end
+  end
+
+  describe "#unfollow" do
+    it "able to unfollow user" do
+      user
+      follower
+
+      follower.follow(user)
+
+      expect(follower.followings.last).to eq(user)
+
+      follower.unfollow(user)
+
+      expect(follower.followings).to be_empty
+    end
+  end
+
+  describe "#following?" do
+    it "able to check following user" do
+      user
+      follower
+
+      follower.follow(user)
+
+      expect(follower.following?(user)).to be_truthy
     end
   end
 end
